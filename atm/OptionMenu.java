43c6ecfcc8c388034c5f4de34784bf9ecb8f29de
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-17 17:54:52 -0600

    chore: set up initial variables

--- atm/OptionMenu.java
+++ atm/OptionMenu.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 
 public class OptionMenu {
+
   Scanner menuInput = new Scanner(System.in);
+  DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+  HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
+
+  
 
 
 
