e9911f429878b513abca9744cfba5557c0bbfe91
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-17 18:09:38 -0600

    feat: add login functionaity

--- atm/OptionMenu.java
+++ atm/OptionMenu.java
@@ -9,11 +9,32 @@ public class OptionMenu {
   DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
   HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
 
-  
-
-
-
   public void getLogin() {
+
+    int x = 1;
+    data.put(123456789, 1111);
+    data.put(987654321, 2222);
+
+    do {
+      try {
+        System.out.println("\n" + "Welcome to ATM machine!" + "\n");
+        // Get Customer Account Number
+        System.out.println("Enter your Account Number: ");
+        int enteredCustomerNumber = menuInput.nextInt();
+        // Get Customer Pin Number
+        System.out.println("Enter your Pin Number: ");
+        int enteredPinNumber = menuInput.nextInt();
+
+        if (data.containsKey(enteredCustomerNumber) && data.containsKey(enteredCustomerNumber).equals(enteredPinNumber)) {
+          // Set customer account number and pin number + open options
+        } else {
+          System.out.println("\n" + "Wrong Customer Number or Pin Number" + "\n");
+        }
+      } catch (Exception e) {
+        System.out.println("\n" + "Invalid character(s). Only numbers are allowed!" + "\n");
+        menuInput.nextLine();
+      }
+    } while (true);
 
   }
 }
